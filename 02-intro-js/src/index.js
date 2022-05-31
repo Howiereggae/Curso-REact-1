6b92106d565ff2f8024faaa565c13195d27479cb
Howiereggae <howiereggae@gmail.com>
2022-05-31 19:29:19 -0400

    Actualizacion

---
+++ 02-intro-js/src/index.js
@@ -0,0 +1,12 @@
+
+
+const persona = {
+  nombre: 'Diolis',
+  apellido: 'Coronado',
+  edad: '31',
+  }
+
+console.log( persona);
+//console.table( persona ); Abre como una tabla de exell
+
+const persona2 = {...persona };
