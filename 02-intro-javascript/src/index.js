d6208e51dd7014e82656a2a7ceb27180da7d03a2
Howiereggae <howiereggae@gmail.com>
2022-06-15 06:29:48 -0300

    Pausa

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -5,6 +5,15 @@
 
 import { heroes } from './data/heroes'
 
-console.log( heroes );
+const getHeroById = (id) => heroes.find( ( heroe ) => heroe.id === id );
+
+console.log(getHeroById(2));
+
+
+
+const getHeroByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner
+
+console.log( getHeroByOwner('Marvel') )
+
 
 
